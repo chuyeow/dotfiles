732e9bfe9ca5f28b265c990e98d6e62b2b6bd329
Cheah Chu Yeow <chuyeow@gmail.com>
2013-11-18 15:56:49 +0800

    I heard giving the host more CPUs is actually better.

--- system/env.zsh
+++ system/env.zsh
@@ -3,7 +3,7 @@ export EDITOR=/usr/bin/vim
 export GREP_OPTIONS='--color=auto'
 
 # Nitrous.IO VM.
-export DEV_COOKBOOK_VM_CPUS=4
+export DEV_COOKBOOK_VM_CPUS=2
 export DEV_COOKBOOK_VM_MEM=3072
 export DEV_COOKBOOK_WORKSPACE=/Data/working_copies/nitrous-workspace
 
