fa9b65c28a5de59d12269d775a22e9a851203f36
Cheah Chu Yeow <chuyeow@gmail.com>
2013-11-19 06:31:07 +0000

    Add path to local git install.

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH=~/bin:~/.local/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$DOTFILES/bin:$PATH
+export PATH=~/bin:~/.local/bin:~/.local/git/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$DOTFILES/bin:$PATH
