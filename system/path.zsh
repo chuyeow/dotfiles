54b66870154f1fd4f417b276ab550e6a12b50cfc
Cheah Chu Yeow <chuyeow@gmail.com>
2013-12-15 07:14:44 +0000

    Don't add system paths to PATH.

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH=~/bin:~/.local/bin:~/.local/git/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$DOTFILES/bin:$PATH
+export PATH=~/bin:~/.local/bin:~/.local/git/bin:DOTFILES/bin:$PATH
 export PATH="/usr/local/heroku/bin:$PATH" # Heroku toolbelt.
