6e9abc6f7f987fc439630675dafb647338cd25fc
Cheah Chu Yeow <chuyeow@gmail.com>
2014-01-13 14:51:21 +0800

    Sync dev-cookbook workspace between host and guest.

--- system/env.zsh
+++ system/env.zsh
@@ -5,5 +5,6 @@ export GREP_OPTIONS='--color=auto'
 # Nitrous.IO VM.
 export DEV_COOKBOOK_VM_CPUS=2
 export DEV_COOKBOOK_VM_MEM=3072
+export DEV_COOKBOOK_SYNC_WORKSPACE=1
 export DEV_COOKBOOK_WORKSPACE=/Data/working_copies/nitrous-workspace
 
